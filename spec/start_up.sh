68e3ba017d4eb8d644e02925a2dc32687c881319
lucasKO2810 <lucaskneffel@Lucass-MBP-2.fritz.box>
2023-04-08 13:16:54 +0200

    delted comments

--- spec/start_up.sh
+++ spec/start_up.sh
@@ -8,9 +8,7 @@ echo "Building the Backend"
 cd saleor-platform
 docker compose build
 docker compose run --rm api python3 manage.py migrate
-#docker compose run --rm api python3 manage.py createsuperuser
 docker compose run --rm api python3 manage.py collectstatic --noinput
-#docker compose run --rm api python3 manage.py populatedb
 docker compose run --rm api python3 manage.py populatedb --createsuperuser
 
 echo "****** Waiting for ${DELAY} seconds for containers to go up ******"
