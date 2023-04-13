3bae5c2632e097626b9c68e08cb2310b7957a0ef
Karen Meliksetian <karen.meliksetian@deep5.io>
2023-04-13 17:00:41 +0200

    added search app

--- spec/start_up.sh
+++ spec/start_up.sh
@@ -8,7 +8,6 @@ echo "Building the Backend"
 cd saleor-platform
 docker compose build
 docker compose run --rm api python3 manage.py migrate
-docker compose run --rm api python3 manage.py collectstatic --noinput
 docker compose run --rm api python3 manage.py populatedb --createsuperuser
 
 echo "****** Waiting for ${DELAY} seconds for containers to go up ******"
